0f82c62cb9bca9ce0a39607604e314afdd659dd9
26krkz <26krkz@gmail.com>
2024-06-23 17:21:22 +0900

    feat: header実装

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,14 @@
+import Link from "next/link";
 import styles from "./page.module.css";
 
 export default function Home() {
-  return <main className={styles.main}>test</main>;
+  return (
+    <main className={styles.main}>
+      <header className={styles.header}>
+        <Link className={styles.headerLink} href="/">
+          <h1>Bookshelf</h1>
+        </Link>
+      </header>
+    </main>
+  );
 }
