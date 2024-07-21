92098ebe64f01568782e2c968efdf2522552a932
26krkz <26krkz@gmail.com>
2024-07-21 11:32:18 +0900

    chore: uiディレクトリを_componentsに変更

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import "./reset.css";
 import { AppRouterCacheProvider } from "@mui/material-nextjs/v13-appRouter";
 import { ThemeProvider } from "@mui/material/styles";
 import theme from "./theme";
-import Header from "./ui/Header";
+import Header from "./_components/Header";
 
 export const metadata: Metadata = {
   title: "Bookshelf",
