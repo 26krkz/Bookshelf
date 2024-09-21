af767c7498ecc7b9c612dbe2419a29a23fa320b1
26krkz <26krkz@gmail.com>
2024-09-21 12:27:37 +0900

    fix: bookviewmodalのcssを変更

--- app/(site)/bookshelfEdit/_components/BookViewModal/index.tsx
+++ app/(site)/bookshelfEdit/_components/BookViewModal/index.tsx
@@ -2,11 +2,19 @@
 import { BookCard } from "@/types";
 import styles from "./styles.module.css";
 import Image from "next/image";
-import { Dialog } from "@radix-ui/themes";
+import { ChevronDownIcon, Dialog } from "@radix-ui/themes";
 import FavoriteButtonContainer from "@/_components/FavoriteButtonContainer";
+import Button from "@/_components/Button";
+import { ChevronUpIcon, Cross1Icon } from "@radix-ui/react-icons";
+import { useState } from "react";
+import clsx from "clsx";
 
 export const BookViewModal = ({ card, favoriteStatus = false }: { card: BookCard; favoriteStatus?: boolean }) => {
   const { title, authors, description, imageUrl, id } = card;
+  const [open, setOpen] = useState(false);
+  const handleClick = () => {
+    setOpen((prev) => !prev);
+  };
   return (
     <Dialog.Content>
       <div className={styles.container}>
@@ -18,12 +26,17 @@ export const BookViewModal = ({ card, favoriteStatus = false }: { card: BookCard
         <div className={styles.modalTextArea}>
           <Dialog.Title>{title}</Dialog.Title>
           <p className={styles.author}>{authors?.[0]}</p>
-          <Dialog.Description className={styles.description}>{description}</Dialog.Description>
+          <Dialog.Description className={clsx(open ? styles.descriptionOpen : styles.description)} dangerouslySetInnerHTML={{ __html: description }} />
+          <Button type="button" onClick={handleClick} className={styles.chevronButton}>
+            {open ? <ChevronUpIcon /> : <ChevronDownIcon />}
+          </Button>
           <FavoriteButtonContainer bookId={id} favorite={favoriteStatus} />
-          <Dialog.Close>
-            <button>閉じる</button>
-          </Dialog.Close>
         </div>
+        <Dialog.Close>
+          <Button type="button" className={styles.closeButton}>
+            <Cross1Icon />
+          </Button>
+        </Dialog.Close>
       </div>
     </Dialog.Content>
   );
